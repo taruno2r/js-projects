722a0eb0eef6825d706e82c9e5e5b0bce27e698a
Tarun <tarun.ottur@gmail.com>
2024-06-01 11:04:46 +0530

    Added a different method to reverse the string

--- palindrome-checker-310524/script.js
+++ palindrome-checker-310524/script.js
@@ -1,14 +1,30 @@
 const input=document.getElementById("input");
 
+
+// METHOD 1
+// function reverseStr(str)
+// {
+//   return str.split("").reverse().join("");
+// }
+
+// METHOD 2
+let reverse;
+
 function reverseStr(str)
 {
-  return str.split("").reverse().join("");
+  let splitStr=str.split("");
+  let reverse="";
+  for(let i=str.length-1;i>=0;i--)
+    {
+      reverse+=splitStr[i];
+    }
+  return reverse;
 }
 
 function check()
 {
   const value=input.value;
-  const reverse=reverseStr(value);
+  reverse=reverseStr(value);
   if (value===null || value==="")
     {
       alert("Enter something");
